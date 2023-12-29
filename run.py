eb93fa08fa880ebd3d513d3dcb15381de76f9edd
Iamrazing <enrique.dechegoyen@gmail.com>
2023-12-29 10:40:22 -0300

    adding missing file

--- run.py
+++ run.py
@@ -4,4 +4,14 @@ from embeddings.embeddings import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    # Use Gunicorn as the server
+    from gunicorn.app.wsgiapp import WSGIApplication
+    from gunicorn.workers.gthread import GThreadWorker
+
+    options = {
+        'bind': '0.0.0.0:5000',
+        'workers': 4,  # Adjust the number of workers based on your needs
+        'worker_class': GThreadWorker,
+    }
+
+    WSGIApplication(app, options).run()
