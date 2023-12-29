a2ce574d9d35e3ef0e0cc57ed53a3a66853abffb
Iamrazing <enrique.dechegoyen@gmail.com>
2023-12-29 09:43:18 -0300

    model up

--- embeddings/embeddings.py
+++ embeddings/embeddings.py
@@ -1,50 +1,45 @@
-# embeddings.py
 from fastembed.embedding import FlagEmbedding as Embedding
 from typing import List
 from decimal import Decimal
-import numpy as np
-from flask import Flask, request, jsonify  # Import jsonify for converting to JSON
+from flask import Flask, request, jsonify
 
 def create_app():
     app = Flask(__name__)
-    # Create a temporary directory for the TensorFlow Hub cache
+
+    # Move the instantiation of embedding_model outside the route handler
+    embedding_model = Embedding(model_name="BAAI/bge-base-en", max_length=512)
 
     @app.route('/embedding', methods=['POST'])
-    def hello_world():
-        data = request.json  # Assuming the data is in JSON format
-        text = data.get('text', 'Guest')  # Retrieve 'text' from the JSON data, default to 'Guest' if not present
-        embedding_model = Embedding(model_name="BAAI/bge-base-en", max_length=512)
+    def get_embedding_route():
+        try:
+            data = request.json
+            text = data.get('text', 'Guest')
 
-        # Get embeddings
-        embeddings = get_embedding([text],embedding_model)
+            # Get embeddings
+            embeddings = get_embedding([text], embedding_model)
 
-        # Convert Decimal embeddings to string for JSON serialization
-        str_embeddings = [str(embed) for embed in embeddings]
+            # Convert Decimal embeddings to string for JSON serialization
+            str_embeddings = [str(embed) for embed in embeddings]
 
-        # Return the result as JSON
-        return embeddings
+            # Return the result as JSON
+            return jsonify(result=str_embeddings)
+
+        except Exception as e:
+            return jsonify(error=str(e)), 400  # Return a JSON response with error message and status code 400 for bad request
 
     return app
 
-def get_embedding(documents: List[str],embedding_model) -> List[Decimal]:
-    """
-    Get embeddings for a list of documents using the fastembed library.
+def get_embedding(documents: List[str], embedding_model) -> List[Decimal]:
+    try:
+        # Get embeddings for the documents
+        embeddings = list(embedding_model.embed(documents))
 
-    Parameters:
-    - documents (List[str]): A list of documents.
+        # Convert embeddings to decimal representation
+        result = float_to_decimal(embeddings[0].tolist())
+        return result
 
-    Returns:
-    - List[Decimal]: A list of decimal representations of embeddings corresponding to the input documents.
-    """
-    # Create an instance of the Embedding model
-
-    # Get embeddings for the documents
-    embeddings = list(embedding_model.embed(documents))
-    print(type(embeddings))
-    # Convert embeddings to decimal representation
-
-    result = float_to_decimal(embeddings[0].tolist())
-    return result
+    except Exception as e:
+        raise RuntimeError(f"Error in embedding process: {str(e)}")
 
 def float_to_decimal(obj):
     if isinstance(obj, float):
